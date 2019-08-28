ca3849643d3e6810b373421df0e492ced9ac6b4c
McClay <mcclay.design@gmail.com>
2019-08-28 10:43:01 -0300

    chore: Commit inicial

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  /** MD5 String = meetUpChaveDeSeguranca27Ago219 */
+  secret: '85bbe93872c2efadef16e96ede44cbcd',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+/**
+routes.get('/', (req, res) => {
+  return res.status(400).json({ Sucess: 'Ok Certo' });
+});
+*/
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+
+export default routes;
